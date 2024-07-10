e4b0d12aac6bde264c9c77b39b50ee30535ee064
sangfei99 <903206051@qq.com>
2024-07-10 17:42:12 +0800

    力扣第104题：二叉树的最大深度

---
+++ src/simple/MaxDepth.java
@@ -0,0 +1,51 @@
+package simple;
+/*
+104. 二叉树的最大深度
+给定一个二叉树 root ，返回其最大深度。
+二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+示例 1：
+输入：root = [3,9,20,null,null,15,7]
+输出：3
+
+示例 2：
+输入：root = [1,null,2]
+输出：2
+ */
+public  class MaxDepth {
+      public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth,rightDepth)+1;
+    }
+
+    public static void main(String[] args) {
+        MaxDepth solution = new MaxDepth();
+        // 示例 1
+        TreeNode root1 = new TreeNode(3);
+        root1.left = new TreeNode(9);
+        root1.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+        System.out.println(solution.maxDepth(root1)); // 输出: 3
+
+        // 示例 2
+        TreeNode root2 = new TreeNode(1);
+        root2.right = new TreeNode(2);
+        System.out.println(solution.maxDepth(root2)); // 输出: 2
+    }
+}
