98572f6386f5363c2776419122cc347b90b2f5a5
sangfei99 <903206051@qq.com>
2024-07-16 15:08:11 +0800

    力扣第169题：多数元素

---
+++ src/simple/MajorityElement.java
@@ -0,0 +1,39 @@
+package simple;
+
+/*
+169. 多数元素
+给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ? n/2 ? 的元素。
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+示例 1：
+输入：nums = [3,2,3]
+输出：3
+
+示例 2：
+输入：nums = [2,2,1,1,1,2,2]
+输出：2
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int condidate = 0;
+        int count =0;
+        for (int num:nums){
+            if (count == 0) {
+                condidate = num;
+            }
+            count += (num==condidate) ? 1 : -1;
+        }
+        return condidate;
+    }
+
+    public static void main(String[] args) {
+        MajorityElement solution = new MajorityElement();
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+        int[] nums3 = {1};
+
+        System.out.println(solution.majorityElement(nums1));  // 输出：3
+        System.out.println(solution.majorityElement(nums2));  // 输出：2
+        System.out.println(solution.majorityElement(nums3));  // 输出：1
+    }
+}
