588a1f8eb2f390cc2f068e9a6eeab4b407009ea6
sangfei99 <903206051@qq.com>
2024-07-29 13:52:13 +0800

    力扣第226题:翻转二叉树

---
+++ src/simple/InvertTree.java
@@ -0,0 +1,71 @@
+package simple;
+
+
+/*
+226. 翻转二叉树
+给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+示例 1：
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+
+示例 2：
+输入：root = [2,1,3]
+输出：[2,3,1]
+
+示例 3：
+输入：root = []
+输出：[]
+ */
+public class InvertTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        //交换左右子树
+        TreeNode temp = root.left;
+        root.left=root.right;
+        root.right = temp;
+
+        //递归翻转左右子树
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static void printPreOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        printPreOrder(root.left);
+        printPreOrder(root.right);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        // 翻转二叉树
+        InvertTree solution = new InvertTree();
+        TreeNode invertedRoot = solution.invertTree(root);
+        printPreOrder(invertedRoot); // 输出应为：4 7 9 6 2 3 1
+    }
+}
