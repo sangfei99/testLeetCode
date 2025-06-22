5cb2135dd18af0a7019b8e237a9f9f8154ccae42
sangfei <903206051@qq.com>
2025-06-22 15:23:42 +0800

    力扣第2题：两数相加

---
+++ src/medium/AddTwoNumbers.java
@@ -0,0 +1,87 @@
+package medium;
+
+import simple.MergeTwoLists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/*
+2.两数相加
+给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例 1：
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+
+示例 2：
+输入：l1 = [0], l2 = [0]
+输出：[0]
+
+示例 3：
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1]
+ */
+public class AddTwoNumbers {
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode (){}
+        ListNode (int val){
+            this.val = val;
+        }
+        ListNode (int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        int carry = 0;
+
+        while (l1 !=null || l2 !=null || carry!=0){
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            curr.next = new ListNode(sum % 10);
+            carry = sum/10;
+
+            curr = curr.next;
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode res = addTwoNumbers(l1,l2);
+        printList(res);
+    }
+
+    //打印链表的方法
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
